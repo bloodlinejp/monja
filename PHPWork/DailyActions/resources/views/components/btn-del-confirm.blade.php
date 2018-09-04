bde5721c51bd10b5871351414aa9fa2db3aab648
bloodlinejp <fumi.bloodlinejp@gmail.com>
2018-09-04 21:29:59 +0900

    削除ボタンモーダルウィンドウのコメント修正

--- PHPWork/DailyActions/resources/views/components/btn-del-confirm.blade.php
+++ PHPWork/DailyActions/resources/views/components/btn-del-confirm.blade.php
@@ -1,25 +1,25 @@
-<!-- 1.モーダル表示のためのボタン -->
+<!-- 1.モーダル表示用ボタン -->
 <button class="btn btn-danger" data-toggle="modal" data-target="#del-confirmation">
     {{ __('actionitems.Delete') }}
 </button>
 
-<!-- 2.モーダルの配置 -->
+<!-- 2.モーダル配置 -->
 <div class="modal fade" id="del-confirmation" tabindex="-1">
     <div class="modal-dialog"> 
-        <!-- 3.モーダルのコンテンツ -->
+        <!-- 3.モーダルコンテンツ -->
         <div class="modal-content">
-            <!-- 4.モーダルのヘッダ -->
+            <!-- 4.モーダルヘッダ -->
             <div class="modal-header">
                 <h4 class="modal-title" id="modal-label">確認</h4>
                 <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <!-- 5.モーダルのボディ -->
+            <!-- 5.モーダルボディ -->
             <div class="modal-body">
                 削除してもよろしいですか？
             </div>
-            <!-- 6.モーダルのフッタ -->
+            <!-- 6.モーダルフッタ -->
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
                 <form style="display:inline" action="{{ url($table.'/'.$id) }}" method="post">
