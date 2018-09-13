33c6b0df767761681cb3396b37097d8b83ce2829
bloodlinejp <fumi.bloodlinejp@gmail.com>
2018-09-13 23:34:34 +0900

    actionitems.js入力チェックに引っかか方場合submitを中断するよう修正

--- PHPWork/DailyActions/public/js/actionitems.js
+++ PHPWork/DailyActions/public/js/actionitems.js
@@ -1,13 +1,21 @@
-//action-items Viewで使用するJS
+// action-items Viewで使用するJS
+
+// 未入力チェック
 $(function () {
-  $("#action-items-create").click(function () {
-    if ($("#from").prop("checked") == false &&
-      $("#to").prop("checked") == false &&
-      $("#text").prop("checked") == false &&
-      $("#value").prop("checked") == false &&
-      $("#checkbox").prop("checked") == false
+  $('#action-items-create').click(function () {
+    if ($('#from').prop('checked') == false &&
+      $('#to').prop('checked') == false &&
+      $('#text').prop('checked') == false &&
+      $('#value').prop('checked') == false &&
+      $('#checkbox').prop('checked') == false
     ) {
-      $("#action-items-create").text("チェックされていません");
+      $('#action-items-create').text('未選択あり');
+      $('#action-items-create').removeClass('btn-primary').addClass('btn-danger');
+      $('html,body').animate({scrollTop:$('#from').offset().top});
+      return false;
+    }
+    else {
+      $(this).parent.submit();
     }
   })
 });
--- PHPWork/DailyActions/public/js/actions.js
+++ PHPWork/DailyActions/public/js/actions.js
@@ -1,5 +1,6 @@
 //actions Viewで使用するJS
 
+//日付select変更時に起動
 $(function () {
   $('select[name=date]').change(function() {
     if ($(this).val() != "") {
