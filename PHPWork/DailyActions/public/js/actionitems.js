7ab7978d4bbee06cff896082c69a8a173111a7c3
bloodlinejp <31733502+bloodlinejp@users.noreply.github.com>
2018-09-14 13:06:31 +0900

    actionitems.jsセレクタの条件修正

--- PHPWork/DailyActions/public/js/actionitems.js
+++ PHPWork/DailyActions/public/js/actionitems.js
@@ -18,10 +18,8 @@ $(function () {
 });
 
 // チェックボックスチェック時の動作
-$('input[type=checkbox]').change(function() {
-  if ($(this).prop('checked') == true &&
-      $(this).attr('name') != 'text2use' &&
-      $(this).attr('name') != 'text3use'   {
+$('input[type=checkbox]' + 'input[name!=index2use]' + 'input[name!=index3use]').change(function() {
+  if ($(this).prop('checked') == true {
     $('.rel').text('');
       $('#action-items-create').removeClass('btn-danger').addClass('btn-primary').text('登録');
   }
